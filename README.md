8c3470bcf9669916abe896a8067ea7f077d18805
José Suárez <josegsuarezg@gmail.com>
2022-01-20 10:37:41 -0300

    Commit inicial del proyecto

---
+++ README.md
@@ -0,0 +1,7 @@
+** Para solventar los problemas con Tailwindcss
+
+npx tailwindcss -i ./src/index.css -o ./src/dist/output.css --watch
+
+** Colocar en el main.jsx
+
+import './dist/output.css';
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <div>
+      <h1>CRM</h1>
+    </div>
+  )
+}
+
+export default App
